a595acca944fdca4d843203157e5f22acb799808
Denis <denismunene999@gmail.com>
2023-08-17 09:03:12 +0300

    Add fallback for polyfill module

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,13 @@
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+
 module.exports = function override(config, env) {
-  // do stuff with the webpack config...
+  // Add the NodePolyfillPlugin
   config.plugins.push(new NodePolyfillPlugin());
+
+  // Add the resolve fallback for timers
+  config.resolve.fallback = {
+    timers: require.resolve("timers-browserify")
+  };
+
   return config;
 };

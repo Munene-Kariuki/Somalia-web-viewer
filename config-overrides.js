b39741cb1b27e18a2ccba13017531019ef333e7d
Denis <denismunene999@gmail.com>
2023-08-17 09:34:15 +0300

    Test

--- config-overrides.js
+++ config-overrides.js
@@ -4,10 +4,5 @@ module.exports = function override(config, env) {
   // Add the NodePolyfillPlugin
   config.plugins.push(new NodePolyfillPlugin());
 
-  // Add the resolve fallback for timers
-  config.resolve.fallback = {
-    timers: require.resolve("timers-browserify")
-  };
-
   return config;
 };
